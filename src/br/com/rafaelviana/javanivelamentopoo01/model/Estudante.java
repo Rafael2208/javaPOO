c93f2771ae342ed371ce2bdbc39e758b4f3ff0ae
Rafael Viana <rafaelviana544@yahoo.com>
2021-06-04 14:46:46 -0300

    javapoo

--- src/br/com/rafaelviana/javanivelamentopoo01/model/Estudante.java
+++ src/br/com/rafaelviana/javanivelamentopoo01/model/Estudante.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.rafaelviana.javanivelamentopoo01.model;
 
-/**
- *
- * @author ailso
- */
-public class Estudante {
+public class Estudante extends Pessoa {
+    
+    private int ra;
+    private String curso;
+
+    
+    public int getRa() {
+        return ra;
+    }
+
+    
+    public void setRa(int ra) {
+        this.ra = ra;
+    }
+
+    
+    public String getCurso() {
+        return curso;
+    }
+
+    
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
     
 }
