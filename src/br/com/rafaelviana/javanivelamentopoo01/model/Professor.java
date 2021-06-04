eb32fb1209cdd6a68be47313e90493edf437ef89
Rafael Viana <rafaelviana544@yahoo.com>
2021-06-04 14:11:53 -0300

    javapoo

---
+++ src/br/com/rafaelviana/javanivelamentopoo01/model/Professor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.rafaelviana.javanivelamentopoo01.model;
+
+/**
+ *
+ * @author ailso
+ */
+public class Professor {
+    
+}
