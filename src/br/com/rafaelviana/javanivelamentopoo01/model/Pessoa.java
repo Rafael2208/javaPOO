36dc927681bb67c4b119e1234f35da6afc7ca17f
Rafael Viana <rafaelviana544@yahoo.com>
2021-06-04 13:40:27 -0300

    javapoo

---
+++ src/br/com/rafaelviana/javanivelamentopoo01/model/Pessoa.java
@@ -0,0 +1,41 @@
+
+package br.com.rafaelviana.javanivelamentopoo01.model;
+
+public class Pessoa {
+    private String nome;
+    private String telefone;
+    private String email;
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    public String getTelefone() {
+        return telefone;
+    }
+
+    
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    
+    public String getEmail() {
+        return email;
+    }
+
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
