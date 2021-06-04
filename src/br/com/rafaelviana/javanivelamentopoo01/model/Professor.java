f08e18f23686d516411ac47989e4234c97e95875
Rafael Viana <rafaelviana544@yahoo.com>
2021-06-04 14:46:38 -0300

    javapoo

--- src/br/com/rafaelviana/javanivelamentopoo01/model/Professor.java
+++ src/br/com/rafaelviana/javanivelamentopoo01/model/Professor.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.rafaelviana.javanivelamentopoo01.model;
 
-/**
- *
- * @author ailso
- */
-public class Professor {
+public class Professor extends Pessoa {
+    
+    private String titulo;
+
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
     
 }
