a0a6a76b255df6af1bc44d964357a415657fbe2e
Rafael Viana <rafaelviana544@yahoo.com>
2021-06-04 14:46:26 -0300

    javapoo

--- src/br/com/rafaelviana/javanivelamentopoo01/view/JavaNivelamentoPOO01.java
+++ src/br/com/rafaelviana/javanivelamentopoo01/view/JavaNivelamentoPOO01.java
@@ -1,7 +1,9 @@
 
 package br.com.rafaelviana.javanivelamentopoo01.view;
 
+import br.com.rafaelviana.javanivelamentopoo01.model.Estudante;
 import br.com.rafaelviana.javanivelamentopoo01.model.Pessoa;
+import br.com.rafaelviana.javanivelamentopoo01.model.Professor;
 
 public class JavaNivelamentoPOO01 {
 
@@ -16,7 +18,7 @@ public class JavaNivelamentoPOO01 {
         System.out.println("Telefone:"+objeto.getTelefone());
         System.out.println("Email:"+objeto.getEmail());
         
-        
+        System.out.println("*****************************************");
         Pessoa neia = new Pessoa ();
         neia.setNome("Neia");
         neia.setTelefone("(11)981911012");
@@ -25,6 +27,24 @@ public class JavaNivelamentoPOO01 {
         System.out.println("Nome:"+neia.getNome());
         System.out.println("Telefone:"+neia.getTelefone());
         System.out.println("Email:"+neia.getEmail());
+        
+        System.out.println("*****************************************");
+        Estudante estudante = new Estudante ();
+        estudante.setNome("Maria");
+        estudante.setRa(123);
+        
+        System.out.println("Nome:"+estudante.getNome());
+        System.out.println("RA:"+estudante.getRa());
+        
+        System.out.println("*****************************************");
+        Professor professor = new Professor ();
+       professor.setNome("Mario");
+       professor.setTitulo("Desenvolvedor");
+       
+        System.out.println("Nome:"+professor.getNome());
+        System.out.println("Titulo:"+professor.getTitulo());
+        
+        
     }
     
 }
